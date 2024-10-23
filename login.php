35afa775c97e51dd86e252195a76e5c7d375ea18
ReemWSW <widsawa.d31@gmail.com>
2024-10-23 14:59:53 +0700

    create login page

---
+++ login.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <title>เข้าสู่ระบบ</title>
+    <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class=" card col-md-4 ">
+                <div class=" card-body">
+                    <h2 class="card-title text-center">เข้าสู่ระบบ</h2>
+                    <form>
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" for="username" class="form-control" placeholder="กรุณาใส่ username" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" for="password" class="form-control" placeholder="กรุณาใส่ password" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary ">เข้าสู่ระบบ</button>
+                    </form>
+                    <div class="text-center">
+                        <a href="register.php">สมัครสมาชิก</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
