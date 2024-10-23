3ae635923c9cfba74af117ed83e9d456aa580289
ReemWSW <widsawa.d31@gmail.com>
2024-10-23 15:00:13 +0700

    create register page

---
+++ register.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+    <title>สมัครสมาชิก</title>
+    <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.rtl.min.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title text-center">
+                            <h2>สมัครสมาชิก</h2>
+                        </div>
+                        <form method="post" action="register.php">
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" class="form-control" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control" required>
+                            </div>
+                            <div class="text-start mt-3">
+                                <button class="btn btn-primary">สมัครสมาชิก</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <link rel="stylesheet" href="bootstrap-5.3.3-dist/js/bootstrap.rtl.min.js">
+
+</body>
+
+</html>
